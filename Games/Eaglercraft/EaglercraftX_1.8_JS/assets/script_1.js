5b8e4c42a759b23b5f18f3a450e0e23f5670a234
Kyguy2.0 <107652213+Kyguyog@users.noreply.github.com>
2026-09-16 16:25:17 -0600

    remove old eaglercrafts

--- Games/Eaglercraft/EaglercraftX_1.8_JS/assets/script_1.js
+++
@@ -1,7 +0,0 @@
-
-"use strict";
-(function(){
-	function eaglerBundleUnwrap(tagIn) { var e = document.getElementById(tagIn); var ret = e.innerText; document.head.removeChild(e); return ret;  }
-	window.eaglercraftXClientSignature = eaglerBundleUnwrap("eaglercraftXClientSignature");
-	window.eaglercraftXClientBundle = eaglerBundleUnwrap("eaglercraftXClientBundle");
-})();
