5f2e9776196beb1f060dba2a0ebcc7dec2215439
Kyguy2.0 <107652213+Kyguyog@users.noreply.github.com>
2026-09-21 11:35:06 -0600

    hopefully Fix Hollow Knight

--- Games/Hollow Knight/code/unity-loader.js
+++ Games/Hollow Knight/code/unity-loader.js
@@ -69,7 +69,7 @@ function createUnityInstance(t, n, l) {
     e(n, "productVersion", "1.0"),
     n)
         b[o] = n[o];
-    b.streamingAssetsUrl = new URL("StreamingAssets",document.querySelector('base').href).href;
+    b.streamingAssetsUrl = new URL("StreamingAssets",document.querySelector('base')?document.querySelector('base').href:document.baseURI).href;
     var a = b.disabledCanvasEvents.slice();
     function i(e) {
         e.preventDefault()
